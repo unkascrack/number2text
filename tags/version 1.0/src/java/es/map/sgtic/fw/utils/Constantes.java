2c562f2d742f915d23f856ae779dbb29362e91ea
carlos.alonso.gonzalez <carlos.alonso.gonzalez@c40d8dce-30c1-11de-9e80-b52b74bdfc52>
2009-04-24 11:35:42 +0000

    versión 1.0 del proyecto

---
+++ tags/version 1.0/src/java/es/map/sgtic/fw/utils/Constantes.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package es.map.sgtic.fw.utils;
+
+/**
+ * 
+ * @author <a href="mailto:carlos.alonso1@map.es">carlos.alonso1</a>
+ * @version 1.0 Fecha: 25/02/2008
+ */
+abstract class Constantes {
+
+	/**
+	 * 
+	 */
+	static final String MENOS = "menos ";
+	static final String Y = " y";
+	static final String CON = " con";
+
+	/**
+	 * 
+	 */
+	static final String CERO = " cero";
+	static final String UNO = " uno";
+	static final String UNIDAD = " un";
+	static final String MIL = " mil";
+	static final String VEINTIUNO = " veintiuno";
+	static final String VEINTIUN = " veintiún";
+	static final String CIEN = " cien";
+
+	/**
+	 * 
+	 */
+	static final String[] MILLARES_SINGULAR = { "", " mil", " millón", " mil",
+			" billón", " mil", " trillón", " mil", " cuatrillón", " mil",
+			" quintillón", " mil", " sestillón", " mil", " septillón", " mil",
+			" octillón", " mil", " novillón", " mil", " decillón", " mil",
+			" oncillón", " mil", " docillón", " mil", " trecillón", " mil",
+			" catorcillón", " mil", " quincillón", " mil", " dieciseismillón",
+			" mil", " diecisietemillón", " mil", " dieciochomillón", " mil" };
+
+	static final String[] MILLARES_PLURAL = { "", " mil", " millones", " mil",
+			" billones", " mil", " trillones", " mil", " cuatrillones", " mil",
+			" quintillones", " mil", " sestillones", " mil", " septillones",
+			" mil", " octillones", " mil", " novillones", " mil",
+			" decillones", " mil", " oncillones", " mil", " docillones",
+			" mil", " trecillones", " mil", " catorcillones", " mil",
+			" quincillones", " mil", " dieciseismillones", " mil",
+			" diecisietemillones", " mil", " dieciochomillones", " mil" };
+
+	static final String[] CENTENAS = { "", " ciento", " doscientos",
+			" trescientos", " cuatrocientos", " quinientos", " seiscientos",
+			" setecientos", " ochocientos", " novecientos" };
+
+	static final String[] DECIMAS = { "", " diez", " veinte", " treinta",
+			" cuarenta", " cincuenta", " sesenta", " setenta", " ochenta",
+			" noventa" };
+
+	static final String[] UNIDADES = { "", " uno", " dos", " tres", " cuatro",
+			" cinco", " seis", " siete", " ocho", " nueve", " diez", " once",
+			" doce", " trece", " catorce", " quince", " dieciséis",
+			" diecisiete", " dieciocho", " diecinueve", " veinte",
+			" veintiuno", " veintidós", " veintitrés", " veinticuatro",
+			" veinticinco", " veintiséis", " veintisiete", " veintiocho",
+			" veintinueve" };
+}
